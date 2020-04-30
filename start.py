0399c552082044359a2a3e14d7922f1838c93c20
Jaylee89 <121769651@qq.com>
2020-05-01 00:25:30 +0800

    template init

--- start.py
+++ start.py
@@ -5,7 +5,7 @@ import contextlib
 import os, re, subprocess, sys
 
 def log(s):
-    sys.stderr.write("start to run:")
+    sys.stderr.write("start to run: ")
     sys.stderr.write(s)
     sys.stderr.write("\n")
     sys.stderr.flush()
@@ -15,13 +15,13 @@ if __name__ == "__main__":
     group = parser.add_mutually_exclusive_group()
     group.add_argument("-v", "--verbose", action="store_true")
     group.add_argument("-q", "--quiet", action="store_true")
-    parser.add_argument("-ey", "--entity", choices=["MY", "SG", "TW", "VN"], help="choice entity")
-    parser.add_argument("-ev", "--env", choices=["cert", "prod"], help="choice env")
+    parser.add_argument("-ey", "--entity", choices=["AU", "SG"], help="choice entity")
+    parser.add_argument("-ev", "--env", choices=["sit", "uat"], help="choice env")
     args = parser.parse_args()
 
     if args.quiet:
         sys.exit()
     elif args.entity and args.env:
-        print("entity is {}， env is {}".format(args.entity, args.env))
+        log("entity is {}， env is {}".format(args.entity, args.env))
     else:
-        print("\nTry '-h' to get more. \n")
+        log("\nTry './start -h' to get more. \n")
